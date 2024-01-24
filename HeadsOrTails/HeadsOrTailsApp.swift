d45b3991c6dbfc0077c78b7d6ea48395fdcc65a8
Snow37 <110422108+Snow37@users.noreply.github.com>
2024-01-24 11:09:35 +0300

    Initial Commit

---
+++ HeadsOrTails/HeadsOrTailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadsOrTailsApp.swift
+//  HeadsOrTails
+//
+//  Created by Ярослав Любиченко on 24.1.2024.
+//
+
+import SwiftUI
+
+@main
+struct HeadsOrTailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
